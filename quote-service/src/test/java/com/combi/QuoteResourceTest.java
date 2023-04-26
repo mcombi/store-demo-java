7d9bcc9eda7c2a215512b6dcbc80a8e552b1505c
Matteo <mcombi@redhat.com>
2023-04-26 11:15:42 +0200

    separate namespace service

--- quote-service/src/test/java/com/combi/QuoteResourceTest.java
+++ quote-service/src/test/java/com/combi/QuoteResourceTest.java
@@ -9,13 +9,5 @@ import static org.hamcrest.CoreMatchers.is;
 @QuarkusTest
 public class QuoteResourceTest {
 
-    @Test
-    public void testHelloEndpoint() {
-        given()
-          .when().get("/quote")
-          .then()
-             .statusCode(200)
-             .body(is("Hello RESTEasy"));
-    }
 
 }
